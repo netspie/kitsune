10013355fe2ae97634f4b49776efd3d2d179553d
drlsn <dariusz.lusnia@gmail.com>
2023-08-29 17:12:17 +0200

    Refactor a bit

--- src/Manabu.UI.Common/Components/CoreComponent.cs
+++ src/Manabu.UI.Common/Components/CoreComponent.cs
@@ -1,5 +1,8 @@
-﻿using Corelibs.Basic.UseCases;
+﻿using Corelibs.Basic.Blocks;
+using Corelibs.Basic.UseCases;
+using Manabu.Entities.Phrases;
 using Manabu.UI.Common.Auth;
+using Manabu.UseCases.Phrases;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using static MudBlazor.CategoryTypes;
@@ -19,8 +22,32 @@ public abstract class CoreComponent : Microsoft.AspNetCore.Components.ComponentB
     protected override async Task OnInitializedAsync()
     {
         _isAdmin = await Auth.IsAdmin();
+
+        await RefreshViewModel();
         await OnInitializedAsyncImpl();
     }
 
     protected virtual Task OnInitializedAsyncImpl() => Task.CompletedTask;
+
+    protected async Task<bool> ExecuteAdminViewAction(Func<Task<Result>> action)
+    {
+        if (!_isAdmin)
+            return false;
+
+        var result = await action();
+
+        await RefreshView();
+
+        return result.IsSuccess;
+    }
+
+    protected async Task<bool> RefreshView()
+    {
+        await RefreshViewModel();
+        await InvokeAsync(StateHasChanged);
+
+        return true;
+    }
+
+    protected virtual Task RefreshViewModel() { return Task.CompletedTask; }
 }
