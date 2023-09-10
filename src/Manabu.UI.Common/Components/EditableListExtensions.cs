44306399c97e1a6f1b838876685c40244c50ad15
drlsn <dariusz.lusnia@gmail.com>
2023-09-10 11:47:46 +0200

    Improve looks

---
+++ src/Manabu.UI.Common/Components/EditableListExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Corelibs.Basic.Collections;
+
+namespace Manabu.UI.Common.Components;
+
+internal static class EditableListExtensions
+{
+    public static string GetIndexStr(this EditableList.TreeItemData item)
+    {
+        return item.GetIndexes()
+            .Select(i => (i + 1).ToString())
+            .AggregateOrDefault((x, y) => $"{x}.{y}");
+    }
+
+    public static int[] GetIndexes(this EditableList.TreeItemData item) =>
+        item.Flatten(i =>
+        {
+            if (i?.Parent is null)
+                return Array.Empty<EditableList.TreeItemData>();
+
+            return i.Parent.SingleToArray();
+        })
+        .Append(item)
+        .Select(i => i.Index)
+        .ToArray();
+}
