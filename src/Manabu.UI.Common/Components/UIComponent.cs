3177ea50dbcdffe98d3f6d507b888e84427910a5
drlsn <dariusz.lusnia@gmail.com>
2023-09-12 13:51:18 +0200

    Get rid of UIComponents project

---
+++ src/Manabu.UI.Common/Components/UIComponent.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Manabu.UI.Common.Components;
+
+public abstract class UIComponent : ComponentBase
+{
+    public static Func<Task> InvokeVoid(Delegate del, params object[] args)
+    {
+        return () =>
+        {
+            if (del is null)
+                return Task.CompletedTask;
+
+            var task = del.DynamicInvoke(args) as Task;
+            if (task is null)
+                return Task.CompletedTask;
+
+            return task;
+        };
+    }
+
+    public static Func<Task<bool>> InvokeBool(Delegate del, params object[] args)
+    {
+        return () =>
+        {
+            if (del is null)
+                return Task.FromResult(true);
+
+            var task = del.DynamicInvoke(args) as Task<bool>;
+            if (task is null)
+                return Task.FromResult(true);
+
+            return task;
+        };
+    }
+
+    public static Func<Task<T>> Invoke<T>(Delegate del, params object[] args)
+    {
+        return () =>
+        {
+            if (del is null)
+                return Task.FromResult<T>(default);
+
+            var task = del.DynamicInvoke(args) as Task<T>;
+            if (task is null)
+                return Task.FromResult<T>(default);
+
+            return task;
+        };
+    }
+}
