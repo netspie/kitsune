2761c3c798c5a8e1408c693e58f31a0ee3f0df31
drlsn <dariusz.lusnia@gmail.com>
2023-10-19 16:58:14 +0200

    Add initial query to get words

---
+++ src/Manabu.Infrastructure/Contexts/Content/Words/GetWordsQueryHandler.cs
@@ -0,0 +1,55 @@
+﻿using Corelibs.Basic.Blocks;
+using Corelibs.MongoDB;
+using Manabu.Entities.Content.WordMeanings;
+using Manabu.Entities.Content.Words;
+using Manabu.UseCases.Content.Words;
+using Mediator;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Manabu.Infrastructure.CQRS.Content.Lessons;
+
+public class GetWordsQueryHandler : IQueryHandler<GetWordsQuery, Result<GetWordsQueryResponse>>
+{
+    private const int MaxItemLimit = 20;
+
+    private readonly MongoConnection _mongoConnection;
+
+    public GetWordsQueryHandler(
+        MongoConnection mongoConnection)
+    {
+        _mongoConnection = mongoConnection;
+    }
+
+    public async ValueTask<Result<GetWordsQueryResponse>> Handle(GetWordsQuery query, CancellationToken cancellationToken)
+    {
+        var result = Result<GetWordsQueryResponse>.Success();
+
+        var wordsCollection = _mongoConnection.Database.GetCollection<Word>(Word.DefaultCollectionName);
+        var wordsFilter = Builders<Word>.Filter.Empty;
+
+        var wordsHintDict = new Dictionary<string, object>();
+        var wordsHint = new BsonDocument(wordsHintDict);
+
+        //if (query.SortArgs)
+        //wordsHint.Add
+
+        var words = await wordsCollection
+            .Aggregate(new AggregateOptions() { })
+            .Lookup<WordMeaning, LookupResult>(
+                foreignCollectionName: WordMeaning.DefaultCollectionName,
+                localField: nameof(Word.Meanings),
+                foreignField: nameof(WordMeaning.Id),
+                @as: nameof(LookupResult.Meanings))
+            .Limit(Math.Min(query.Limit, MaxItemLimit))
+            .ToListAsync();
+        
+        return result.With(new GetWordsQueryResponse(
+            new WordsDTO(
+                Words: words.Select(w => new WordDTO(w.Word.Id.Value, w.Word.Value, w.Meanings[0].Translations[0], w.Word.PartsOfSpeech[0].Value)).ToArray(),
+                Range: null,
+                IsThereMoreWords: true)));
+    }
+
+    public record LookupResult(Word Word, WordMeaning[] Meanings);
+}
---
+++ src/Manabu.UseCases/Content/Words/GetWordsQuery.cs
@@ -0,0 +1,35 @@
+﻿using Corelibs.Basic.Blocks;
+using Mediator;
+
+namespace Manabu.UseCases.Content.Words;
+
+public record GetWordsQuery(
+    int Limit = 10,
+    RangeDTO? Range = null,
+    SortArg[]? SortArgs = null,
+    FilterArg[]? FilterArgs = null) : IQuery<Result<GetWordsQueryResponse>>;
+
+public record SortArg(
+    string Field,
+    int Order = 1);
+
+public record FilterArg(
+    string Field,
+    string Value);
+
+public record GetWordsQueryResponse(WordsDTO Content);
+
+public record WordsDTO(
+    WordDTO[] Words,
+    RangeDTO Range,
+    bool IsThereMoreWords);
+
+public record WordDTO(
+    string Id,
+    string Value,
+    string Meaning,
+    string PartOfSpeech);
+
+public record RangeDTO(
+    int Start,
+    int? End = null);
