c56cf88fe5c8dd2a3a5565ec0ecd556e95b9840b
drlsn <dariusz.lusnia@gmail.com>
2024-01-04 15:41:10 +0100

    Improve looks

---
+++ src/Manabu.UI.Common/State/WordThemesStates.cs
@@ -0,0 +1,8 @@
+﻿using Manabu.UI.Common.Storage;
+
+namespace Manabu.UI.Common.State;
+
+public class WordThemes_IsOriginalShown(IStorage storage) : StateItem<bool>(storage);
+public class WordThemes_IsMeaningsShown(IStorage storage) : StateItem<bool>(storage);
+public class WordThemes_IsReadingsShown(IStorage storage) : StateItem<bool>(storage);
+public class MainLayout_IsOriginalMenuShown(IStorage storage) : StateItem<bool>(storage);
--- src/Manabu.UI.Common/State/WordThemes_IsMeaningsShown.cs
+++
@@ -1,5 +0,0 @@
-﻿using Manabu.UI.Common.Storage;
-
-namespace Manabu.UI.Common.State;
-
-public class WordThemes_IsMeaningsShown(IStorage storage) : StateItem<bool>(storage);
